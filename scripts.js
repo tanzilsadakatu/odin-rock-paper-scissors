b2c181cb4787608baa7460b7bb588402b4ae4ebf
Sadakatu Tanzil <sadakatutanzil@outlook.com>
2026-05-05 13:12:01 +0600

    added computer choice retrieval method with random number generator

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@ function playerChoice() {
   const choice = prompt(`Pick 1: rock, paper, scissors`);
   return choice.toLowerCase();
 }
+
+function computerChoice() {
+  const options = ["rock", "paper", "scissors"];
+  return options[randomInteger(2)];
+}
+
+function randomInteger(upto = 1) {
+  return Math.round(Math.random() * upto);
+}
